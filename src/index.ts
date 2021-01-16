057c54207bdc6815ee33732a35ff4a8aefb1f25f
pavel-arpa <arepev.p.work@users.noreply.github.com>
2021-01-16 21:29:53 +0300

    require and import ALL

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
+const View = require('./components/slider/slider-view.ts')
+
+
 // IMPORTS
 function importAll(resolve: any): any {
   resolve.keys().forEach(resolve);
 }
 
-importAll(require.context('../src/', true, /\.ts$|\.scss$/));
+importAll(require.context('../src/components', true, /\.ts$|\.scss$/));
+importAll(require.context('../src/styles', true, /\.ts$|\.scss$/));
 // =============================================================
 
-class HelloMessage {
-  constructor(public message: string) {}
-}
+const view = new View('item');
 
-const msg = new HelloMessage("Hi, John!")
-
-console.log(msg)
+(<any>window).s = view;
 
