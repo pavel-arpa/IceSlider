438b9524d88dd23b22d78642988c0fe0af5ba1f4
pavel-arpa <arepev.p.work@users.noreply.github.com>
2021-01-25 11:40:39 +0300

    Updated test file

---
+++ src/test.spec.ts
@@ -0,0 +1,44 @@
+// const jsdom = require("jsdom");
+// const { JSDOM } = jsdom;
+const { ViewPoint, ViewRange} = require('./components/slider/ice-slider_view.ts')
+// const dom = new JSDOM('<html><body id="root"><div id="ice-slider"></div></body></html>')
+
+// global.window = dom.window
+// global.document = dom.window.document
+
+describe('VIEW point: ====================', () => {
+  document.body.innerHTML = '<div id="ice-slider"></div>'
+
+  const _vP = new ViewPoint('ice-slider', {pointSize: 16})
+
+  
+  describe('Main HTMLElement "$el"', () => {
+    test('should be defined', () => {
+      expect(_vP.$points[0].style._values.width).toBe('16px')
+    })
+  })
+
+  describe('Setting up styles', () => {
+    test('size of point', () => {
+      expect(_vP.$el).toBeDefined()
+    })
+  })
+
+  describe('toDragHorizon', () => {
+    test('should be defined', () => {
+      expect(_vP.toDragHorizon).toBeDefined()
+    })
+  })
+
+  describe('moveItem', () => {
+    test('should be defined', () => {
+      expect(_vP.moveItem).toBeDefined()
+    })
+  })
+
+  describe('moveAtCenter', () => {
+    test('should be defined', () => {
+      expect(_vP.moveItem).toBeDefined()
+    })
+  })
+})
