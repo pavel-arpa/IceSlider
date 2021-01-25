64ca54cc872ee1a05fbe3bd323536630f23c97c0
pavel-arpa <arepev.p.work@users.noreply.github.com>
2021-01-25 11:39:45 +0300

    Added rendring inner html code + setup options

--- src/components/slider/ice-slider_view.ts
+++ src/components/slider/ice-slider_view.ts
@@ -1,25 +1,52 @@
-interface Options {
-  // sliderWidth: number;
+function getTemplate(): string {
+  return `
+  <div class="ice-slider__text-wrapper">
+    <h3 class="ice-slider__title" data-type="is__title">Range slider</h3>
+    <span class="ice-slider__value" data-type="is__current-val">15000</span></div>
+  <div class="ice-slider__slider"><div class="ice-slider__point">
+    </div><div class="ice-slider__range"></div>
+    <div class="ice-slider__line-bg"></div>
+  </div>
+  `
 }
 
-export class View {
-  options: Options;
+interface OptionsPoint {
+  pointSize: number;
+}
+
+export class ViewPoint {
+  options: OptionsPoint;
   $el: HTMLElement;
   $points: NodeListOf<HTMLElement>;
   $lineBg: HTMLElement;
 
-  constructor(id: string, options: Options) {
+  constructor(id: string, options: OptionsPoint) {
+    // PROPERTIES
+    this.options = options;
     this.$el = document.getElementById(id)
+    this.render()
     this.$points = this.$el.querySelectorAll('.ice-slider__point')
     this.$lineBg = this.$el.querySelector('.ice-slider__line-bg')
-
-    this.options = options;
     
     // CALL METHODS
     if(this.$el !== null) {
       this.$points[0].ondragstart = () => false // turn off browser d'n'd
+      this.setup()
       this.toDragHorizon()
+      console.log(document);
+
+      // LISTENERS
+      this.resetPointSize()
     }
+  }
+
+  private render() {
+    this.$el.innerHTML = getTemplate()
+  }
+
+  private setup() {
+    this.$points[0].style.width = this.$points[0].style.height = this.options.pointSize + 'px'
+    this.$points[0].style.left = this.$points[0].offsetLeft - this.options.pointSize / 2 + 'px'
   }
 
   toDragHorizon() {
@@ -51,12 +78,50 @@ export class View {
     } else {
       this.$points[0].style.left = offsetLeftSliderSECOND + 'px';
     }
+    
   }
 
   removeMoveItem(event) {
     document.removeEventListener('mousemove', this.moveAtCenter)
   }
+
+  resizeWindowHadler(event) {
+    let pointOffsetLeft: number = this.$points[0].offsetLeft - this.$points[0].offsetWidth / 2
+    let lineBgOffsetLeft: number = this.$lineBg.offsetLeft
+    let difference: number = pointOffsetLeft - lineBgOffsetLeft
+
+    let lineBgWidth: number = this.$lineBg.offsetWidth
+    // let relativePosition: number = 
+
+
+
+    // console.log(difference)
+    
+  }
+
+  resetPointSize() {
+    this.resizeWindowHadler = this.resizeWindowHadler.bind(this)
+    window.addEventListener('resize', this.resizeWindowHadler)
+  }
+
 }
 
 
-module.exports = View
+
+// =====================================================================
+export class ViewRange {
+  $el: HTMLElement;
+
+  constructor(id: string) {
+    this.$el = document.getElementById(id)
+
+  }
+}
+
+
+
+
+
+
+
+module.exports = { ViewPoint, ViewRange }
